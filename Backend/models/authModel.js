2ee2603b8e49fa4eb24dd0b01b31eb03c90ecf05
selemondev <selemondev19@gmail.com>
2022-11-15 19:09:12 +0300

    :white_check_mark: add auth model

---
+++ Backend/models/authModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const authSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Please add an email address"],
+    },
+
+    username: {
+        type: String,
+        required: [true, "Please add a username"]
+    },
+
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        unique: true
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+
+    image: {
+        type: String
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("auth", authSchema)
