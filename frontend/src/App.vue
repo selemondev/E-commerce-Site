412db31cf6b47971d2c6253e4b2ebac5cfbd2401
selemondev <selemondev19@gmail.com>
2022-11-17 08:54:36 +0300

    :rocket: frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p class="text-red-500 font-bold">Hello World</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
---
+++ frontend/src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import { createPinia } from "pinia";
+import "./assets/style.css"
+import App from './App.vue';
+import router from "./router";
+const app = createApp(App)
+const pinia = createPinia();
+app.use(pinia)
+app.use(router)
+app.mount('#app')
