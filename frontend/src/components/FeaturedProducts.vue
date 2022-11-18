5d7a54d39cf44bc018c5899746750bd529e3598d
selemondev <selemondev19@gmail.com>
2022-11-18 18:25:08 +0300

    :bug: fix: dropdown-bug

--- frontend/src/components/FeaturedProducts.vue
+++ frontend/src/components/FeaturedProducts.vue
@@ -10,6 +10,7 @@ const productStore = useProductStore();
 const userInput = ref<string>("");
 watchEffect(() => {
     productStore.getCategoryProducts(userInput.value);
+    console.log(userInput.value)
     productStore.getProducts();
 });
 
@@ -27,21 +28,16 @@ watchEffect(() => {
 const genders = [
     {
         id: 1,
-        name: "All"
-    },
-
-    {
-        id: 2,
         name: "men"
     },
 
     {
-        id: 3,
+        id: 2,
         name: "women"
     },
 
     {
-        id: 4,
+        id: 3,
         name: "kids"
     }
 ];
@@ -51,12 +47,13 @@ const genders = [
         <div class="flex-between mx-6">
             <h1 class="text-xl">New & Featured</h1>
             <select name="genders" class="py-1 px-1 rounded-md border border-gray-600" v-model="userInput">
-                <option value="All">All</option>
+                <option value="" disabled>All</option>
                 <option v-for="gender in genders" :value="gender.name" :key="gender.id">{{ gender.name }}</option>
             </select>
         </div>
         <div class="flex-center my-4">
-            <div v-if="!userInput" v-for="product in products" :key="product._id" class="mx-6 space-y-3">
+
+            <div v-if="!userInput.valueOf" v-for="product in products" :key="product._id" class="mx-6 space-y-3">
                 <img :src="product.image" :alt="product.title" class="w-80 h-80">
                 <div class="space-y-1">
                     <div class="flex-between">
@@ -67,18 +64,7 @@ const genders = [
                 </div>
             </div>
 
-            <div v-else-if="userInput === 'All'" v-for="product in products" :key="product.title" class="mx-6 space-y-3">
-                <img :src="product.image" :alt="product.title" class="w-80 h-80">
-                <div class="space-y-1">
-                    <div class="flex-between">
-                        <h2>{{ product.title }}</h2>
-                        <p>{{ product.price }}</p>
-                    </div>
-                    <p class="text-gray-600">{{ product.desc }}</p>
-                </div>
-            </div>
-
-            <div v-else v-for="product in categoryProducts" :key="product._id" class="mx-6 space-y-3">
+            <div v-else v-for="product in categoryProducts" :key="product.desc" class="mx-6 space-y-3">
                 <img :src="product.image" :alt="product.title" class="w-80 h-80">
                 <div class="space-y-1">
                     <div class="flex-between">
