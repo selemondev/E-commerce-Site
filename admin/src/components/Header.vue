aa944fa2963b82ba3bd1a1d49f27236931af7763
selemondev <selemondev19@gmail.com>
2022-11-19 21:50:06 +0300

    :white_check_marker: add admin header

---
+++ admin/src/components/Header.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import SearchInput from './SearchInput.vue';
+</script>
+<template>
+    <div>
+        <SearchInput/>
+    </div>
+</template>
--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{vue,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "Poppins": "Poppins"
+      }
+    },
   },
   plugins: [],
 }
