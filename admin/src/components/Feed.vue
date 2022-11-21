ac409ad9aaa1f279104726c36ffaee840e542356
selemondev <selemondev19@gmail.com>
2022-11-21 22:28:48 +0300

    :fire: remove: remove admin

--- admin/src/components/Feed.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import Header from "../components/Header.vue"
-</script>
-<template>
-    <div class="border-r border-gray-200 flex-grow max-w-2xl sm:ml-[73px] xl:ml-[300px]">
-        <Header/>
-    </div>
-</template>
--- admin/src/components/Header.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import SearchInput from './SearchInput.vue';
-</script>
-<template>
-    <div>
-        <SearchInput/>
-    </div>
-</template>
