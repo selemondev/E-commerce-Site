86a6792c6f1aa683ca26af946ff9a15ad6cf0aac
selemondev <selemondev19@gmail.com>
2022-11-23 08:46:00 +0300

    :wrench: configure dashboard layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,16 @@
 <script lang="ts" setup>
 import Navbar from './components/Navbar.vue';
+import DashboardView from './views/DashboardView.vue';
+import { ref } from "vue";
+const token = ref(false)
 </script>
 <template>
   <div>
-    <Navbar/>
-    <router-view/>
+    <Navbar />
+    <router-view v-slot="{ Component, route }">
+        <component v-if="route.name === 'dashboard'" :is="Component" />
+        <component :is="Component" />
+    </router-view>
   </div>
 </template>
 <style>
